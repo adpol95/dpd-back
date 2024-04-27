ca9094243995b17c9c5e924a4a9d69fb826c55d9
adpol95 <adpol95@gmail.com>
2024-04-27 23:15:19 +0300

    Добавлена возможность получать пользователей по шаблонам с вложенными объектами

--- modules/clients/Router.js
+++ modules/clients/Router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const clientsAll = require("./getAllClients");
+const clientsResponsible = require("./getResponsibleClients");
 
 const router = Router();
 
 router.get('/', clientsAll);
+router.post('/test', clientsResponsible);
 
 module.exports = router;
--- modules/clients/getResponsibleClients.js
+++ modules/clients/getResponsibleClients.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const clientModel = mongoose.model('Clients', Clients);
 
 module.exports = function (req, res) {
-    clientModel.find({responsibleFullName: req.body.responsible})
+    clientModel.find({ "name.first": req.body.title })
         .exec()
         .then((resp) => res.status(200).json(resp))
         .catch((err) => res.status(400).json(err))
